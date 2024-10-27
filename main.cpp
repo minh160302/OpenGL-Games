ac6308c000ee3e8c375edeb595bfc62f1fae94ca
Minh Nguyen <minh160302@gmail.com>
2024-10-26 17:38:36 -0700

    draw a sprite

--- main.cpp
+++ main.cpp
@@ -1,12 +1,95 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <cstdint>
 #include <cstdio>
 
 void error_callback(int error, const char *description) {
   fprintf(stderr, "Error: %s\n", description);
 }
 
+//* Structs */
+struct Buffer {
+  size_t width, height;
+  uint32_t *data;
+};
+
+struct Sprite {
+  size_t width, height;
+  uint8_t *data;
+};
+
+//** Helper Functions */
+void buffer_sprite_draw(Buffer *buffer, const Sprite &sprite, size_t x,
+                        size_t y, uint32_t color) {
+  for (size_t xi = 0; xi < sprite.width; ++xi) {
+    for (size_t yi = 0; yi < sprite.height; ++yi) {
+      size_t sy = sprite.height - 1 + y - yi;
+      size_t sx = x + xi;
+      if (sprite.data[yi * sprite.width + xi] && sy < buffer->height &&
+          sx < buffer->width) {
+        buffer->data[sy * buffer->width + sx] = color;
+      }
+    }
+  }
+}
+
+bool sprite_overlap_check(const Sprite &sp_a, size_t x_a, size_t y_a,
+                          const Sprite &sp_b, size_t x_b, size_t y_b) {
+  // NOTE: For simplicity we just check for overlap of the sprite
+  // rectangles. Instead, if the rectangles overlap, we should
+  // further check if any pixel of sprite A overlap with any of
+  // sprite B.
+  if (x_a < x_b + sp_b.width && x_a + sp_a.width > x_b &&
+      y_a < y_b + sp_b.height && y_a + sp_a.height > y_b) {
+    return true;
+  }
+
+  return false;
+}
+
+void validate_shader(GLuint shader, const char *file = 0) {
+  static const unsigned int BUFFER_SIZE = 512;
+  char buffer[BUFFER_SIZE];
+  GLsizei length = 0;
+
+  glGetShaderInfoLog(shader, BUFFER_SIZE, &length, buffer);
+
+  if (length > 0) {
+    printf("Shader %d(%s) compile error: %s\n", shader, (file ? file : ""),
+           buffer);
+  }
+}
+
+bool validate_program(GLuint program) {
+  static const GLsizei BUFFER_SIZE = 512;
+  GLchar buffer[BUFFER_SIZE];
+  GLsizei length = 0;
+
+  glGetProgramInfoLog(program, BUFFER_SIZE, &length, buffer);
+
+  if (length > 0) {
+    printf("Program %d link error: %s\n", program, buffer);
+    return false;
+  }
+
+  return true;
+}
+
+uint32_t rgb_to_uint32(uint8_t r, uint8_t g, uint8_t b) {
+  return (r << 24) | (g << 16) | (b << 8) | 255;
+}
+
+void buffer_clear(Buffer *buffer, uint32_t color) {
+  for (size_t i = 0; i < buffer->width * buffer->height; ++i) {
+    buffer->data[i] = color;
+  }
+}
+
+//** Main */
 int main(int argc, char const *argv[]) {
+  const size_t buffer_width = 224;
+  const size_t buffer_height = 256;
+
   glfwSetErrorCallback(error_callback);
 
   GLFWwindow *window;
@@ -21,7 +104,7 @@ int main(int argc, char const *argv[]) {
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
   //* Create a windowed-mode window and its OpenGL context */
-  window = glfwCreateWindow(640, 480, "Title", NULL, NULL);
+  window = glfwCreateWindow(buffer_width, buffer_height, "Title", NULL, NULL);
   if (!window) {
     glfwTerminate();
     return -1;
@@ -29,19 +112,152 @@ int main(int argc, char const *argv[]) {
 
   glfwMakeContextCurrent(window);
 
+  GLenum err = glewInit();
+  if (err != GLEW_OK) {
+    fprintf(stderr, "Error initializing GLEW.\n");
+    glfwTerminate();
+    return -1;
+  }
+
   // Call OpenGL
   int glVersion[2] = {-1, 1};
   glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
   glGetIntegerv(GL_MAJOR_VERSION, &glVersion[1]);
 
   printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
+  printf("Renderer used: %s\n", glGetString(GL_RENDERER));
+  printf("Shading Language: %s\n", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
   glClearColor(1.0, 0.0, 0.0, 1.0);
+
+  //* Graphics buffer */
+  Buffer buffer;
+  buffer.width = buffer_width;
+  buffer.height = buffer_height;
+  buffer.data = new uint32_t[buffer.width * buffer.height];
+
+  buffer_clear(&buffer, 0);
+
+  //* Texture for presenting buffer to OpenGL */
+  GLuint buffer_texture;
+  glGenTextures(1, &buffer_texture);
+  // specify image format and standard parameters
+  glBindTexture(GL_TEXTURE_2D, buffer_texture);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, buffer.width, buffer.height, 0,
+               GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  // vertex array object (VAO) for generating fullscreen triangle
+  GLuint fullscreen_triangle_vao;
+  glGenVertexArrays(1, &fullscreen_triangle_vao);
+  // glBindVertexArray(fullscreen_triangle_vao);
+
+  // Shaders for displaying buffers
+  const char *fragment_shader =
+      "\n"
+      "#version 330\n"
+      "\n"
+      "uniform sampler2D buffer;\n"
+      "noperspective in vec2 TexCoord;\n"
+      "\n"
+      "out vec3 outColor;\n"
+      "\n"
+      "void main(void){\n"
+      "    outColor = texture(buffer, TexCoord).rgb;\n"
+      "}\n";
+
+  const char *vertex_shader =
+      "\n"
+      "#version 330\n"
+      "\n"
+      "noperspective out vec2 TexCoord;\n"
+      "\n"
+      "void main(void){\n"
+      "\n"
+      "    TexCoord.x = (gl_VertexID == 2)? 2.0: 0.0;\n"
+      "    TexCoord.y = (gl_VertexID == 1)? 2.0: 0.0;\n"
+      "    \n"
+      "    gl_Position = vec4(2.0 * TexCoord - 1.0, 0.0, 1.0);\n"
+      "}\n";
+
+  // Compile 2 shaders into code the GPU can understand and linked into a shader
+  // program
+  GLuint shader_id = glCreateProgram();
+  // Create vertex shader
+  {
+    GLuint shader_vp = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(shader_vp, 1, &vertex_shader, 0);
+    glCompileShader(shader_vp);
+    validate_shader(shader_vp, vertex_shader);
+    glAttachShader(shader_id, shader_vp);
+
+    glDeleteShader(shader_vp);
+  }
+  // Create fragment shader
+  {
+    GLuint shader_fp = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(shader_fp, 1, &fragment_shader, 0);
+    glCompileShader(shader_fp);
+    validate_shader(shader_fp, fragment_shader);
+    glAttachShader(shader_id, shader_fp);
+
+    glDeleteShader(shader_fp);
+  }
+
+  glLinkProgram(shader_id);
+
+  if (!validate_program(shader_id)) {
+    fprintf(stderr, "Error while validating shader.\n");
+    glfwTerminate();
+    glDeleteVertexArrays(1, &fullscreen_triangle_vao);
+    delete[] buffer.data;
+    return -1;
+  }
+
+  glUseProgram(shader_id);
+
+  GLint location = glGetUniformLocation(shader_id, "buffer");
+  glUniform1i(location, 0);
+
+  // OpenGL setup for Buffer Display
+  glDisable(GL_DEPTH_TEST);
+  glBindVertexArray(fullscreen_triangle_vao);
+
+  // Game
+  Sprite alien_sprite;
+  alien_sprite.width = 11;
+  alien_sprite.height = 8;
+  alien_sprite.data = new uint8_t[88]{
+      0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, // ..@.....@..
+      0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, // ...@...@...
+      0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, // ..@@@@@@@..
+      0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, // .@@.@@@.@@.
+      1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, // @@@@@@@@@@@
+      1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, // @.@@@@@@@.@
+      1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, // @.@.....@.@
+      0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0  // ...@@.@@...
+  };
+
+  uint32_t clear_color = rgb_to_uint32(0, 128, 0);
 
   while (!glfwWindowShouldClose(window)) {
     //  double buffering scheme; the "front" buffer is used for displaying an
     //  image, while the "back" buffer is used for drawing
-    glClear(GL_COLOR_BUFFER_BIT);
+
+    buffer_clear(&buffer, clear_color);
+
+    buffer_sprite_draw(&buffer, alien_sprite, 112, 128,
+                       rgb_to_uint32(128, 0, 0));
+
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, buffer.width, buffer.height,
+                    GL_RGBA, GL_UNSIGNED_INT_8_8_8_8, buffer.data);
+
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
     glfwSwapBuffers(window);
 
@@ -50,5 +266,11 @@ int main(int argc, char const *argv[]) {
 
   glfwDestroyWindow(window);
   glfwTerminate();
+
+  glDeleteVertexArrays(1, &fullscreen_triangle_vao);
+
+  delete[] alien_sprite.data;
+  delete[] buffer.data;
+
   return 0;
 }
